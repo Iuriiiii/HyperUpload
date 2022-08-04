c3f9e1a66ce7f04d06680dfd5570d3ec72035d54
Alejandra Olazagasti <102782029+AlejandraOlazagasti@users.noreply.github.com>
2022-08-03 22:30:08 -0600

    "header"

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "../components/root.css"
+import "../components/header.css"
 
-const header = () => {
+const Header = () => {
   return (
-    <div>header</div>
+    <header className="header-container">
+      <div className="header__logo">
+        <img src="../assets/logo.png" alt="HyperUpload" />
+      </div>
+      <div className="header__search-container">
+        <button className="header-container__lup">
+          <i className='bx bx-search-alt' ></i>
+        </button>
+        <input className="header-container__input" type="text" placeholder="Search in HyperUpload" className="header__search">
+        
+        </input>
+        <button className="header-container__filter">
+          <i className='bx bx-filter'></i>
+        </button>
+      </div>
+      <div className="header__user">
+        <img src="../assets/user.png" alt="Img User"/>
+      </div>
+    </header>
   )
 }
 
-export default header
+export default Header
