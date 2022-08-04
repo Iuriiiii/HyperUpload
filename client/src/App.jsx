ae8a8c575427dfd6c3ba2d64a6af0e5412107dd6
Reyalic <martinvt360@gmail.com>
2022-08-04 13:41:04 -0500

    login screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Aside from './components/aside'
 import Header from './components/header'
+import LogCard from './components/LogCard'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <LogCard />
       <Aside />
     </div>
   )
---
+++ client/src/components/LogCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LogForm from './LogForm'
+import "../components/login.css"
+import "../components/root.css"
+
+const LogCard = () => {
+  return (
+    <div className='log-card'>
+        <LogForm />
+    </div>
+  )
+}
+
+export default LogCard
