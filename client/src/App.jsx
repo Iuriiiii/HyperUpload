14bf02d6ac3da3a94401585a628bf9fd4350f200
Alejandra Olazagasti <102782029+AlejandraOlazagasti@users.noreply.github.com>
2022-08-04 00:13:12 -0600

    "aside"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Aside from './components/aside'
 import Header from './components/header'
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Aside />
     </div>
   )
 }
